05c660ac3152500a39ad028643757b44f0a77d41
Cody Dirks <cody.dirks@gmail.com>
2025-11-15 13:54:12 -0800

    Fix inference script.

--- simple_inference.py
+++ simple_inference.py
@@ -9,10 +9,7 @@ examples = pd.read_csv("data/future_unseen_examples.csv")
 
 inference_times = []
 for row in tqdm(examples.to_dict(orient="records")):
-    row = examples.iloc[0]
-    data = row.to_dict()
-
-    response = requests.post(INFERENCE_URL, json=data)
+    response = requests.post(INFERENCE_URL, json=row)
     inference_times.append(response.elapsed.total_seconds())
 
 print(f"Average inference time: {sum(inference_times) / len(inference_times)} seconds")
