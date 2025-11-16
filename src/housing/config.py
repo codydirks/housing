4f83242e9d094223263be277c3203a6a8dcebba4
Cody Dirks <cody.dirks@gmail.com>
2025-11-15 20:50:30 -0800

    Clean up config and align path names with those expected in `create_model.py`.

--- src/housing/config.py
+++ src/housing/config.py
@@ -2,12 +2,19 @@ import pathlib
 
 PROJECT_DIR = pathlib.Path(__file__).parent.parent.parent
 DATA_DIR = PROJECT_DIR / "data"
-ZIPCODE_DEMOGRAPHICS_PATH = DATA_DIR / "zipcode_demographics.csv"
+SALES_PATH = DATA_DIR / "kc_house_data.csv"
+DEMOGRAPHICS_PATH = DATA_DIR / "zipcode_demographics.csv"
 
 MODEL_DIR = PROJECT_DIR / "model"
 MODEL_PATH = MODEL_DIR / "model.pkl"
 
 INFERENCE_COLUMNS = [
-    'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
-    'sqft_above', 'sqft_basement', 'zipcode'
+    'bedrooms',
+    'bathrooms',
+    'sqft_living',
+    'sqft_lot',
+    'floors',
+    'sqft_above',
+    'sqft_basement',
+    'zipcode'
 ]
