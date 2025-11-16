0aa4c579c0200165e0858d3bbfa1d535f47020bc
Cody Dirks <cody.dirks@gmail.com>
2025-11-16 14:34:06 -0800

    Update inference test script to test both prod and dev endpoints.

--- simple_inference.py
+++ simple_inference.py
@@ -1,15 +1,36 @@
-import time
+from typing import List
 from tqdm.auto import tqdm
+
 import requests
 import pandas as pd
 
-FULL_INFERENCE_URL = "http://localhost:8000/inference/full"
+HOST = "http://localhost:8000"
 
-examples = pd.read_csv("data/future_unseen_examples.csv")
+PRODUCTION_INFERENCE_URL = f"{HOST}/inference/production/full"
+DEV_INFERENCE_URL = f"{HOST}/inference/dev/full"
 
-inference_times = []
-for row in tqdm(examples.to_dict(orient="records")):
-    response = requests.post(FULL_INFERENCE_URL, json=row)
-    inference_times.append(response.elapsed.total_seconds())
 
-print(f"Average inference time: {sum(inference_times) / len(inference_times)} seconds")
+def print_results(inference_times: List[float], label: str):
+    average_time = sum(inference_times) / len(inference_times)
+    print(f"Average {label} inference time: {average_time} seconds")
+
+
+def test_endpoint(url: str, examples: pd.DataFrame):
+    inference_times = []
+    for row in tqdm(examples.to_dict(orient="records")):
+        response = requests.post(url, json=row)
+        inference_times.append(response.elapsed.total_seconds())
+
+    label = "production" if "production" in url else "dev"
+    print_results(inference_times, label)
+
+
+def main():
+    examples = pd.read_csv("data/future_unseen_examples.csv")
+
+    for url in [PRODUCTION_INFERENCE_URL, DEV_INFERENCE_URL]:
+        test_endpoint(url, examples)
+
+
+if __name__ == "__main__":
+    main()
